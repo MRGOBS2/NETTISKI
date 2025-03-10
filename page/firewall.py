5d24b1606c87543f79d977e6cf9f9aa5c95266ec
nurrifai0103 <nurrifai0103@gmail.com>
2025-03-10 20:40:28 +0700

    cek

--- page/firewall.py
+++ page/firewall.py
@@ -5,21 +5,45 @@ import time
 
 def show(klien):
     st.title("Konfigurasi Firewall")
-    
+
+    ### BLOKIR IP 
     st.subheader("Blokir IP")
-    #memisahkan ip dengan baris
-    #list ip
-    blocked_ip = st.text_area("Masukkan IP Address untuk diblokir (pisahkan dengan baris baru):").split('\n') 
+    blocked_ips = st.text_area("Masukkan IP Address untuk diblokir (pisahkan dengan koma atau baris baru):")
+    blocked_ip_list = [ip.strip() for ip in blocked_ips.replace('\n', ',').split(',') if ip.strip()]
+
     if st.button("Blokir IP"):
-        for ip in blocked_ip: #untuk setiap ip di list
+        for ip in blocked_ip_list:
             command = f'/ip firewall filter add chain=forward src-address={ip} action=drop comment="Block {ip}"'
             stdin, stdout, stderr = klien.exec_command(command)
             error = stderr.read().decode().strip()
             if error:
-                return error
+                st.error(f"Gagal memblokir IP: {ip} - {error}")
             else:
                 st.success(f"Berhasil memblokir IP: {ip}")
+
         time.sleep(2)
         st.rerun()
-    
-   
+
+    ### BLOKIR DOMAIN 
+    st.subheader("Blokir Domain")
+    blocked_domains = st.text_area("Masukkan Domain untuk diblokir (pisahkan dengan koma atau baris baru):")
+    blocked_domain_list = [domain.strip() for domain in blocked_domains.replace('\n', ',').split(',') if domain.strip()]
+
+    if st.button("Blokir Domain"):
+        for domain in blocked_domain_list:
+
+            perintah = [
+                f'/ip firewall address-list add list=BLOCKED-DOMAINS address={domain} timeout=1d',
+                f'/ip firewall filter add chain=forward dst-address-list=BLOCKED-DOMAINS action=drop comment="Block {domain}"'
+            ]
+
+            for command in perintah:
+                stdin, stdout, stderr = klien.exec_command(command)
+                error = stderr.read().decode().strip()
+                if error:
+                    st.error(f"Gagal memblokir {domain}: {error}")
+                else:
+                    st.success(f"Berhasil menambahkan {domain} ke daftar blokir")
+
+        time.sleep(2)
+        st.rerun()
